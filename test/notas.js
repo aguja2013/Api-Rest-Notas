83e429a207c800ee93c592b5461f98c15b2ca178
Agustin Palmieri <aguja13@hotmail.com>
2015-09-21 10:13:05 -0300

    server

--- test/notas.js
+++ test/notas.js
@@ -1,8 +1,43 @@
-describe('recurso/notas', function() {
+var request = require('supertest')
+var api = require('../server.js')
+// correr las pruebas con diferentes hosts:
+var host = process.env.API_TEST_HOST || api
+
+request = request(host)
+
+
+describe('recurso /notas', function() {
 
   describe('POST', function() {
-  	it('debería crear una nota nueva')
-  	it('debería retornar una nota con id nuevo')
+  	it('deberia crear una nota', function() {
+      var data = {
+        "nota": {
+          "title": "Mejorando.la #node-pro",
+          "description": "Introduccion a clase",
+          "type": "js",
+          "body": "soy el cuerpo de json"
+        }
+       }
+
+       // libreria supertest:
+       // hago un request al servidor:
+
+     request
+       .post('/notas')
+// Accept application/json
+       .set('Accept', 'application/json')
+       .send(data)
+// Status Code = 201
+       .expect(201)
+       .expect('Content-Type', /application\/json/)
+       .end(function(err, res) {
+
+         var body = res.body
+
+         // Nota existe
+         expect(body).to.have.property('nota')
+		})
+  	})
 
    })
   })
