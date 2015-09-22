930a476115287c99c1ffe706b58c7f587ec2bee9
Agustin Palmieri <aguja13@hotmail.com>
2015-09-22 07:52:09 -0300

    paso 3

--- test/notas.js
+++ test/notas.js
@@ -50,4 +50,53 @@ describe('recurso /notas', function() {
   	})
 
    })
+
+
+describe('GET', function() {
+  	it('deberia obtener una nota existente', function(done) {
+      var data = {
+        "nota": {
+          "title": "Mejorando.la #node-pro",
+          "description": "Introduccion a clase",
+          "type": "js",
+          "body": "soy el cuerpo de json"
+        }
+       }
+
+       // libreria supertest:
+       // hago un request al servidor:
+       // crear solicitud de http enviando data
+
+     request
+       .post('/notas')
+       .send(data)
+// Accept application/json
+       .set('Accept', 'application/json')
+// Status Code = 201
+       .expect(201)
+       // .expect('Content-Type', /application\/json/)
+       .end(function(err, res) {
+
+         var id = res.body.nota.id
+
+         request
+         	.get('/notas/' + id)
+         	.expect(200)
+         	.expect('Content-Type', /application\/json/)
+         	.end(function(err, res) {
+         		var nota = res.body.notas
+
+         // Propiedades
+         expect(nota).to.have.property('title', 'Mejorando.la #node-pro')
+         expect(nota).to.have.property('description', 'Introduccion a clase')
+         expect(nota).to.have.property('type', 'js')
+         expect(nota).to.have.property('body', 'soy el cuerpo de json')
+         expect(nota).to.have.property('id', id)
+         done()
+		})
+  	})
+
+   })
+
   })
+ })
