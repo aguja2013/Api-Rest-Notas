741871f30f5e37781725e748b0d6ea10edd62bc9
Agustin Palmieri <aguja13@hotmail.com>
2015-09-22 08:32:55 -0300

    paso 4

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var bodyParser = require('body-parser')
 	*/
 
 var server = express()
+var db = {}
 
 /**
 	* middelware
@@ -31,12 +32,26 @@ server.post('/notas', function(req, res){
 	console.log('POST',req.body.nota)
 	var notaNueva = req.body.nota
 	notaNueva.id = 123
+
+	db[notaNueva.id] = notaNueva
+
 	res
 		.status(201)
 		// .send({}) //express al detectar un objeto vacío lo convierte en json
 		.json({
 			nota: notaNueva
 		})
+})
+
+server.get('/notas/:id?', function(req, res){
+	console.log('GET /notas/%s', req.params.id)
+	var id = req.params.id
+	var nota = db[id]
+
+	res.json({
+		notas: nota
+	})
+
 })
 
 
